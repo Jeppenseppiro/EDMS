629f3d5417507289e91c7f7bce60f73d97dc1494
Jeff Jurolan <jeff.jurolan@acutraining.onmicrosoft.com>
2022-05-13 10:31:43 +0800

    App Layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -116,7 +116,7 @@
 
     var today = new Date();
     var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth()+1).padStart(2, '0'); //January is 0!
+    var mm = String(today.getMonth()+01).padStart(2, '0'); //January is 0!
     var yyyy = today.getFullYear();
     today = mm + '/' + dd + '/' + yyyy;
 
