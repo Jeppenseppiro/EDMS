bd4dd3fad6a8e4925a18fa4a5dfdadb922869a0a
Jeff Jurolan <jeffjurolan990@gmail.com>
2022-05-13 10:33:42 +0800

    Update app.blade.php
    - updated getDate today function

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -114,9 +114,11 @@
       minScrollbarLength: 20
     });
 
-    	
-  var today = new Date();
-  var today = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth()+01).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    today = mm + '/' + dd + '/' + yyyy;
 
     // Data Picker Initialization
     $('.datepicker').pickadate({
